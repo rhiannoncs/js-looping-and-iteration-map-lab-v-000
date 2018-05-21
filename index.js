7e00b35c0ec37a05c264d8c9e9cf3a8962cb28a4
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-05-21 12:21:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ function lowerCaseDrivers(drivers) {
   return drivers.map(function (name) { return name.toLowerCase(); });
 }
 
+function splitName(name) {
+  return name.split(" ")
+}
+
 function nameToAttributes(drivers) {
   return drivers.map(function (name) { return Object.assign({}, name, {firstName: })
 }
